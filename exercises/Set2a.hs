f81305321aed8c88c414aeea266bc9bf8781f81c
Michał Cichoń <michal.cichon@codete.com>
2022-06-10 17:56:30 +0200

    Lecture 2: Ex 3

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -46,7 +46,7 @@ takeFinal n xs = drop (length xs - n) xs
 --   updateAt 2 0 [4,5,6,7] ==>  [4,5,0,7]
 
 updateAt :: Int -> a -> [a] -> [a]
-updateAt i x xs = todo
+updateAt i x xs = (take i xs) ++ [x] ++ (drop (i+1) xs)
 
 ------------------------------------------------------------------------------
 -- Ex 4: substring i j s should return the substring of s starting at
