69b01c95026343fb5c3e7cb583f6554c256166ac
Michał Cichoń <michal.cichon@codete.com>
2023-04-01 22:19:32 +0200

    5a.3

--- exercises/Set5a.hs
+++ exercises/Set5a.hs
@@ -51,7 +51,7 @@ twoBananas = MkShoppingEntry "Banana" 1.1 2
 --   totalPrice twoBananas   ==> 2.2
 
 totalPrice :: ShoppingEntry -> Double
-totalPrice = todo
+totalPrice (MkShoppingEntry name price count) = price * fromIntegral count
 
 -- buyOneMore should increment the count in an entry by one
 --
@@ -59,7 +59,7 @@ totalPrice = todo
 --   buyOneMore twoBananas    ==> MkShoppingEntry "Banana" 1.1 3
 
 buyOneMore :: ShoppingEntry -> ShoppingEntry
-buyOneMore = todo
+buyOneMore (MkShoppingEntry name price count) = MkShoppingEntry name price (count+1)
 
 ------------------------------------------------------------------------------
 -- Ex 4: define a datatype Person, which should contain the age (an
